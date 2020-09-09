8551274297b3d84b82c9b0ec7444695faab50079
Anton Belko <30528165+ko1p@users.noreply.github.com>
2020-09-09 11:26:31 +0300

    add hander for Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,21 @@
-import React, {useContext} from "react"
+import React, {useContext, useState} from "react"
 import {AlertContext} from "../context/alert/alertContext"
 
 export const Search = () => {
 
     const {show} = useContext(AlertContext)
 
+    const [value, setValue] = useState('')
+
     const onSubmit = event => {
-        if (event.key === 'Enter') {
-            show('This is our alert!')
+        if (event.key !== 'Enter') {
+            return
+        }
+
+        if (value.trim()) {
+            console.log('make request by value', value)
+        } else {
+            show('Вы не указали данные пользователя!')
         }
     }
 
@@ -17,6 +25,8 @@ export const Search = () => {
                 className="form-control"
                 type="text"
                 placeholder="Введите ник пользователя..."
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
                 onKeyPress={onSubmit}
             />
         </div>
