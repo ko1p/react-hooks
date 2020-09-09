df3dc95d5e999d55ef70cc82b9476424fe9c5913
Anton Belko <30528165+ko1p@users.noreply.github.com>
2020-09-09 12:10:08 +0300

    fill githubstate component

--- src/context/github/GitHubState.js
+++ src/context/github/GitHubState.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {GitHubContext} from './GitHubContext'
 import {gitHubReducer} from "./gitHubReducer";
+import {CLEAR_USERS, GET_REPOS, GET_USER, SET_LOADING} from "../types";
 
 export const gitHubState = ({children}) => {
     const initialState = {
@@ -11,8 +12,41 @@ export const gitHubState = ({children}) => {
     }
     const [state, dispatch] = useReducer(gitHubReducer, initialState)
 
+    const search = async value => {
+        setLoading()
+        /// ...
+
+        dispatch({type: SEARCH_USERS, payload: []})
+    }
+
+    const getUser = async name => {
+        setLoading()
+        // ...
+
+        dispatch({type: GET_USER, payload: {}})
+    }
+
+    const getRepos = async name => {
+        setLoading()
+        // ...
+
+        dispatch({type: GET_REPOS, payload: []})
+    }
+
+    const clearUsers = () => {
+        dispatch({type: CLEAR_USERS})
+    }
+
+    const setLoading = () => {
+        dispatch({type: SET_LOADING})
+    }
+
+    const {user, users, loading, repos} = state;
+
     return (
-        <GitHubContext.Provider value={}>
+        <GitHubContext.Provider value={
+            search, getUser, getRepos, clearUsers, setLoading,
+        }>
             {children}
         </GitHubContext.Provider>
     )
