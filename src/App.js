e4b97bfa24e43220c48e95e0cf53577902fcb895
Anton Belko <30528165+ko1p@users.noreply.github.com>
2020-09-14 11:21:57 +0300

    Start new module - React animations. Create app.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import React from 'react'
-import {BrowserRouter, Switch, Route} from "react-router-dom"
-import {Navbar} from "./components/Navbar"
-import {Home} from "./pages/Home"
-import {About} from "./pages/About"
-import {Profile} from "./pages/Profile"
-import {AlertState} from "./context/alert/AlertState";
-import {GitHubState} from "./context/github/GitHubState"
+import React, {useState} from "react"
 
-function App() {
-  return (
-      <GitHubState>
-          <AlertState>
-              <BrowserRouter>
-                  <Navbar />
-                  <div className="container pt-4">
-                      <Switch>
-                          <Route path="/" exact component={Home} />
-                          <Route path="/about" component={About} />
-                          <Route path="/profile/:name" component={Profile} />
-                      </Switch>
-                  </div>
-              </BrowserRouter>
-          </AlertState>
-      </GitHubState>
-  );
+export default function App () {
+
+    const [toggle, setToggle] = useState(true)
+
+
+    return (
+        <div className="container">
+            <button onClick={() => setToggle(!toggle)}>Toggle</button>
+            <hr/>
+            <div className="blocks">
+                {toggle ? <div className="square blue">{toggle.toString()}</div> : null}
+            </div>
+        </div>
+    )
 }
-
-export default App;
